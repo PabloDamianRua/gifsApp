0f5bfbc336ac74aaa9806b4be055eec194f3b795
PabloDamianRua <pdrua06@gmail.com>
2022-10-24 22:32:48 -0300

    ultimos ajustes

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -9,6 +9,7 @@ import { GifsService } from '../services/gifs.service';
 
 export class BusquedaComponent {
 @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+@ViewChild('txtLimite') txtLimite!:ElementRef<HTMLInputElement>;
  
 
 constructor(private gifService: GifsService)
@@ -18,15 +19,20 @@ constructor(private gifService: GifsService)
 
 buscar(){
   const valor = this.txtBuscar.nativeElement.value;
+  const limite: number = Number(this.txtLimite.nativeElement.value);
   if(valor.trim().length === 0 ){ return;}
+  if(limite == 0 ){ return;}
   
-  this.gifService.buscarGifs(valor);
+  this.gifService.buscarGifs(valor, limite);
   this.seleccionar();
 }
 
+actualizarLimite()
+{
+  this.gifService.actualizarLimite( Number(this.txtLimite.nativeElement.value));
+}
 seleccionar(){
   this.txtBuscar.nativeElement.select();
-  console.log('Select');  
 }
 
 }
